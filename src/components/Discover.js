19a4957324a9f98b15114031a98f601ec4283d3c
Lance Toledo <50975456+lancetoledo@users.noreply.github.com>
2023-05-02 14:35:18 -0400

    ADD: link to facebook on the learn more btn

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -11,7 +11,7 @@ const Discover = () => {
                         <div className='contact'>Contact Us</div>
                         <h1 className='visit'>Come and visit us at the shop</h1>
                         <p className='location'>154 Cooper Rd. West Berlin, NJ 08091</p>
-                        <div className='learn_btn'>Learn More</div>
+                        <a className='learn_btn' href='https://www.facebook.com/spreme.wrps/'>Learn More</a>
                     </div>
                 </div>
             </div>
