cd2fee4c14138da261240462e1b717887b8d94f8
Lance Toledo <50975456+lancetoledo@users.noreply.github.com>
2023-06-12 15:09:50 -0400

    ADD: static data and item_container

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -3,16 +3,26 @@ import Header from '../components/Header'
 import data from '../data.json'
 
 const Shop = () => {
-    console.log(data, "YOOO")
+    // console.log(data)
     return (
         <div className='Shop'>
             <div className='collections'>
                 <Header />
             </div>
-            <div className='items'>
-
+            <div className='items-container'>
+                {/* <h1>YOO</h1> */}
+                {data.map((item) => {
+                    // console.log(item.value)
+                    return <div className='item' key={item.id}>
+                        <img id='item_image' src={item.image} />
+                        <div className='item_info'>
+                            <p id='value'>{item.value}</p>
+                            <p>${item.price?.toFixed(2)}</p>
+                        </div>
+                    </div>
+                })}
             </div>
-        </div>
+        </div >
     )
 }
 
